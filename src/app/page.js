e93174c18beedb43e58d36e7d8168c696d34a076
Sunita Penta <sunitapenta@nw18mac-sunitap.local>
2024-08-09 20:13:47 +0530

    function change

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import MentorsDiv from "@/app/Components/MentorsDiv/MentorsDiv";
 import Footer from "@/app/Components/Footer/Footer";
 
 
-export default function Home() {
+export default function Page() {
     return (
         <>
             <MainDiv/>
